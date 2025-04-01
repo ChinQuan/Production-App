a3d5fadc529d30aedb8d129be29f9b395204f0d3
ChinQuan <kamilmobile16@gmail.com>
2025-04-01 21:52:14 +0100

    app.py

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
 import streamlit as st
 from home import show_home
-from charts import show_charts
 from auth import authenticate_user, logout
-
 
 # Initialize the application
 st.set_page_config(page_title="Production Manager App", layout="wide")
 st.title("Production Manager App")
 
-
-# Initialize user session
 if 'user' not in st.session_state:
     st.session_state.user = None
 
-
-# Login Panel
 if st.session_state.user is None:
     user = authenticate_user()
     if user:
@@ -25,12 +19,8 @@ else:
     if st.sidebar.button("Logout"):
         logout()
 
-
-# Sidebar Navigation
 if st.session_state.user is not None:
-    menu = st.sidebar.radio("Navigation", ["Home", "Production Charts"])
-
+    menu = st.sidebar.radio("Navigation", ["Home"])  # Usunięto opcję "Production Charts"
+    
     if menu == "Home":
         show_home()
-    elif menu == "Production Charts":
-        show_charts()
